753677fce72fee2b0f093d7bdb0872633bc56b6f
areebaahmed <aahmed70@illinois.edu>
2019-04-23 14:03:46 -0500

    we added two new slides

--- app/src/main/java/com/example/triviamemeapp/MainActivity.java
+++ app/src/main/java/com/example/triviamemeapp/MainActivity.java
@@ -1,34 +1,39 @@
 package com.example.triviamemeapp;
 
+import android.content.Intent;
 import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txt;
+    Button single;
+    Button multi;
+    Button bonus;
     private static final String TAG = "Meme";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button single = findViewById(R.id.button);
-        single.setOnClickListener(v -> {
-            Log.d(TAG, "start the single player game");
-            gameStart();
+        single = (Button) findViewById(R.id.button);
+        single.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent(MainActivity.this, directions.xml.class ));
+            }
         });
-        final Button multi = findViewById(R.id.button2);
-        multi.setOnClickListener(v -> {
-            Log.d(TAG, "start the multiplayer game");
-            gameStart();
-        });
-        final Button bonus = findViewById(R.id.button3);
-        bonus.setOnClickListener(v -> {
-            Log.d(TAG, "start the bonus game");
-            gameStart();
-        });
+
     }
     public boolean gameStart() {
         return false;
     }
+    public void sendMessage(View view) {
+
+    }
+
 }
